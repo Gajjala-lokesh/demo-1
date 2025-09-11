d352aa6741d9bdcde75cf1fa6d99c1729ce96457
Gajjala-lokesh <gajjalalokesh403@gmail.com>
2025-09-11 23:20:20 +0530

    Delete two_table.py

--- two_table.py
+++
@@ -1,9 +0,0 @@
-# Print 2 times table
-print("2 Times Table:")
-for i in range(1, 11):
-    print(f"2 x {i} = {2 * i}")
-
-print("\n3 Times Table:")
-# Print 3 times table
-for i in range(1, 11):
-    print(f"3 x {i} = {3 * i}")
